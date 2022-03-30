3a4476a2c7b4097ac408ae0f517b9c3104c42378
Randy King Jr <rkingjr@outlook.com>
2022-03-30 01:24:11 -0500

    add thougths routes

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const {getAllThoughts,getThoughtsById,createThoughts,updateThoughts,deleteThoughts,addReaction,deleteReaction} = require('../../controllers/thoughtsController.js');
+
+router.route('/').get(getAllThoughts);
+router.route('/:id').get(getThoughtsById).put(updateThoughts).delete(deleteThoughts);
+router.route('/:userId').post(createThoughts);
+router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:/thoughtId/reactions/;reactionId').delete(deleteReaction);
+
+module.exports = router;
