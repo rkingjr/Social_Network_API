d980b60556a0d782fa7a2eec205b7d7204b75b7d
Randy King Jr <rkingjr@outlook.com>
2022-03-30 01:55:00 -0500

    fix users db

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,8 @@
-//Require Users Model
 const {users} = require('../models');
 const usersController = {
     createUsers({body},res) {
         users.create(body)
-        .then(dbUsersData => res.json(dbUsersData))
+        .then(dbUsersInfo => res.json(dbUsersInfo))
         .catch(err => res.status(400).json(err));
     },
 
@@ -12,7 +11,7 @@ const usersController = {
         .populate({path:'thoughts',select:'-__v'})
         .populate({path:'friends',select:'-__v'})
         .select('-__v')
-        .then(dbUsersData => res.json(dbUsersData))
+        .then(dbUsersInfo => res.json(dbUsersInfo))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -24,12 +23,12 @@ const usersController = {
         .populate({path:'thoughts',select:'-__v'})
         .populate({path:'friends',select:'-__v'})
         .select('-__v')
-        .then(dbUsersData => {
-            if(!dbUsersData) {
+        .then(dbUsersInfo => {
+            if(!dbUsersInfo) {
                 res.status(404).json({message:'There is no user with this ID!'});
                 return;
             }
-            res.json(dbUsersData)
+            res.json(dbUsersInfo)
         })
         .catch(err => {
             console.log(err);
@@ -40,24 +39,24 @@ const usersController = {
     // Update a current User by ID
     updateUsers({params,body}, res) {
         users.findOneAndUpdate({_id:params.id},body,{new:true,runValidators:true})
-        .then(dbUsersData => {
-            if(!dbUsersData) {
+        .then(dbUsersInfo => {
+            if(!dbUsersInfo) {
                 res.status(404).json({message:'There is no user with this ID!'});
                 return;
             }
-            res.json(dbUsersData);
+            res.json(dbUsersInfo);
         })
         .catch(err => res.json(err))
     },
 
     deleteUsers({params}, res) {
         users.findOneAndDelete({_id:params.id})
-        .then(dbUsersData => {
-            if(!dbUsersData) {
+        .then(dbUsersInfo => {
+            if(!dbUsersInfo) {
                 res.status(404).json({message:'There is no user with this ID!'});
                 return;
             }
-            res.json(dbUsersData);
+            res.json(dbUsersInfo);
         })
         .catch(err => res.status(400).json(err));
     },
@@ -66,12 +65,12 @@ const usersController = {
         users.findOneAndUpdate({_id:params.id},{$push:{friends:params.friendId}},{new: true})
         .populate({path:'friends',select:('-__v')})
         .select('-__v')
-        .then(dbUsersData => {
-            if (!dbUsersData) {
+        .then(dbUsersInfo => {
+            if (!dbUsersInfo) {
                 res.status(404).json({message:'There is no user with this ID!'});
                 return;
             }
-        res.json(dbUsersData);
+        res.json(dbUsersInfo);
         })
         .catch(err => res.json(err));
     },
@@ -80,12 +79,12 @@ const usersController = {
         users.findOneAndUpdate({_id:params.id},{$pull:{friends:params.friendId}},{new: true})
         .populate({path:'friends',select:'-__v'})
         .select('-__v')
-        .then(dbUsersData => {
-            if(!dbUsersData) {
+        .then(dbUsersInfo => {
+            if(!dbUsersInfo) {
                 res.status(404).json({message:'There is no user with this ID!'});
                 return;
             }
-            res.json(dbUsersData);
+            res.json(dbUsersInfo);
         })
         .catch(err => res.status(400).json(err));
     }
